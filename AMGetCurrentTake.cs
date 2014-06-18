5238a43eff421b5e913114d37f04ab981a10a8f1
David Dionisio <garapata@gmail.com>
2014-06-18 00:59:03 -0700

    fixed compile errors with unity 4.5

--- AMGetCurrentTake.cs
+++ AMGetCurrentTake.cs
@@ -3,10 +3,10 @@ using HutongGames.PlayMaker;
 
 namespace M8.PlayMaker {
     [ActionCategory("Mate Animator")]
-    [Tooltip("Get the current take that is playing.")]
+    [HutongGames.PlayMaker.Tooltip("Get the current take that is playing.")]
     public class AMGetCurrentTake : FsmStateAction {
         [RequiredField]
-        [Tooltip("The Game Object to work with. NOTE: The Game Object must have an AnimatorData component attached.")]
+        [HutongGames.PlayMaker.Tooltip("The Game Object to work with. NOTE: The Game Object must have an AnimatorData component attached.")]
         [CheckForComponent(typeof(AnimatorData))]
         public FsmOwnerDefault gameObject;
 
@@ -31,7 +31,7 @@ namespace M8.PlayMaker {
         // Code that runs on entering the state.
         public override void OnEnter() {
             InitData();
-            output.Value = aData.takeName;
+            output.Value = aData.currentPlayingTakeName;
             Finish();
         }
     }
